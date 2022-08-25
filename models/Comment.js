8a84944d027fab2de76ec29beff193edd690412a
Strider <dave.barnes23@gmail.com>
2022-08-25 18:15:38 -0500

    add validators to Pizza, Comment, Reply Schemas and tested

--- models/Comment.js
+++ models/Comment.js
@@ -8,10 +8,13 @@ const ReplySchema = new Schema(
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         createAt: {
             type: Date,
@@ -29,10 +32,14 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
     {
         writtenBy: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         commentBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         createdAt: {
             type: Date,
